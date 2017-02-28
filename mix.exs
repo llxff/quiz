236d027178e1ca8e23dba7cccd915ac8f8fc3c57
Александр Фомин <ll.wg.bin@gmail.com>
2017-02-28 20:41:09 +0300

    bump to elixir 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quiz.Mixfile do
   def project do
     [app: :quiz,
      version: "0.0.1",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -17,9 +17,10 @@ defmodule Quiz.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Quiz, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :absinthe, :absinthe_plug, :absinthe_ecto]]
+    [
+      mod: {Quiz, []},
+      extra_applications: [:logger]
+    ]
   end
 
   # Specifies which paths to compile per environment.
